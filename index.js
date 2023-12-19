e5e040164aee4d44c5741d86fb7e8354b41883c5
robertobeltramiglass <129887456+robertobeltramiglass@users.noreply.github.com>
2023-12-19 22:23:05 +0100

    trim

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { decreaseTimer } from './js/utils.js';
 import { animate } from "./js/gameLogic.js";
 import { keyDownHandler, keyUpHandler } from './js/eventHandlers.js';
-import { player, enemy } from './js/gameObjects.js';
 
 decreaseTimer()
 animate()
